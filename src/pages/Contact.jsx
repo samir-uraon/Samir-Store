199aed90fb4ccf6c8d0c4a11a1c41c296a7c59af
Your Name <you@example.com>
2025-04-09 12:59:32 +0530

    first commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,6 +28,7 @@ return bool
 			<>
 			<Header/>
 <div className='main'>
+	<div className="main2">
 	<div id='ContactHead'><h2>Contact <span style={usstyle}>Me!</span></h2></div>
 
 	<div className="detailsdiv">
@@ -65,7 +66,7 @@ return bool
 </form>
 	</div>
 
-
+	</div>
 	</div>
 	</div>				
 				</>
