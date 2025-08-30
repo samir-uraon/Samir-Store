33106544b61533d58d0ddef48647d08abb09e677
Your Name <you@example.com>
2025-08-30 12:20:48 +0530

    ok

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,10 +4,10 @@ import "./ProductPage.css"
 
 const products = [
   { id: 1, name: "Gold Necklace", price: "₹45,000", image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR4JjGf6OELuGfXT2_Rlhp492_D7uba7tSDP6wGFg9x9VEplbC1WRK5xc8lZw4J8js84dtUkYcNxxGdAF60t0o7RNIA5xomU_Wb7y0id0KK7bFgAnY", category: "Women" },
-  { id: 2, name: "Diamond Ring", price: "₹25,000", image: "https://lh3.googleusercontent.com/gg-dl/AJfQ9KSy9xtCSQldaBASifQ8hoEG3KQOvq6mK4lRNmz5m9qsMbcfvGbGEIEPZT7nI-aI8VJCCTH9lmXbs0aNJbo9JCmP2euFKoJMACutfrdqX2RaYLKv3o4UfvbH9GVQcRbKZf6v072gaCW2Zqy43rmWyoacI7t2Ce_6dT3EtF5db8vMNz3I7g", category: "Women" },
-  { id: 3, name: "Gold Bracelet", price: "₹15,000", image: "https://lh3.googleusercontent.com/gg-dl/AJfQ9KRR1EfqFY-QW_MO00_Uqak2Y0L4krTC2N-OVUlVi2FS9PWy90zI5YDUEiS40ClMVYe2-OI1ZljwFqgnyTfCyNfcg36Q2Y7s3rr_dn0Z93GqQX5EjRHm9TO7d7J1JGVx962xm6xEP4zLDuqfxaK3_i8Y7bfltmEgUKKnaBlvXt7HGKPOHw", category: "Men" },
-  { id: 4, name: "Silver Earrings", price: "₹5,000", image: "https://lh3.googleusercontent.com/gg-dl/AJfQ9KRce2dVgIRVySE06EKp-00Oh7IPkMd-hBpyux4YyPKT3jCTEBV6qhDUkyQ8NlW5g-LPs0MJTwVx8ponAn_NY6dmHiCQsACG_AaYfyj15GbtCgYyBAun_5j8nylcJKGCwusnnsM0gKo20PEIqq-k_NReWx2CGSQNCrGPbxBnoXOpau4E-w", category: "Women" },
-  { id: 5, name: "Men’s Chain", price: "₹18,000", image: "https://lh3.googleusercontent.com/gg-dl/AJfQ9KTY3drzfKUQq-w6cMvxzGbJO9qRvcZTs5tfJZKsA5_h3SIqPVrO_RZIUN_XX178U1kkUH_z1kpsENkwRJtdOL7WdI1ANz2exvB6G0s6kgo5QhUl9J7g-XMiXt6RX3DQDqA7IyLHnaHcXi_xCqtcJOcyxQGjBbvDh6bkMQtN3a3-K6p3Jg", category: "Men" },
+  { id: 2, name: "Diamond Ring", price: "₹25,000", image: "https://www.arthursjewelers.com/content/images/thumbs/Original/31-V218ERR-E-19361275.jpg", category: "Women" },
+  { id: 3, name: "Gold Bracelet", price: "₹15,000", image: "https://5.imimg.com/data5/OO/UM/FN/SELLER-45272018/68d2e2cdb4be8ee660cc53d4cae1ed39.jpg", category: "Men" },
+  { id: 4, name: "Silver Earrings", price: "₹5,000", image: "https://tse3.mm.bing.net/th/id/OIP.yiKofBRUhmqggJpPaCTNMAHaJ4?rs=1&pid=ImgDetMain&o=7&rm=3", category: "Women" },
+  { id: 5, name: "Men’s Chain", price: "₹18,000", image: "https://assets.talisa.com/media/catalog/product/cache/29d7d34990d2aec07d9b88f7ee7d79c4/c/u/curb_chain_men_name_necklace_-_stainless_steel-911.jpg", category: "Men" },
 ]
 
 export default function ProductPage() {
